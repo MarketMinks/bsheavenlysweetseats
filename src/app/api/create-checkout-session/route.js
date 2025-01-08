501c35c5d9019861379db7f0b51ca8546ce8de46
zijunw04 <floatsinkfc@gmail.com>
2025-01-07 19:49:28 -0600

    update

--- src/app/api/create-checkout-session/route.js
+++ src/app/api/create-checkout-session/route.js
@@ -7,6 +7,7 @@ export async function POST(request) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
       apiVersion: '2023-10-16',
     });
+
     
     const { email, cartItems } = await request.json();
 

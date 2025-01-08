e664353e7d2806617972a5147fcfc67f31409a05
Garry Rodgers <garryrodgers20050@gmail.com>
2025-01-07 19:27:33 -0600

    Demo1

--- src/app/about/about.jsx
+++ src/app/about/about.jsx
@@ -8,7 +8,7 @@ export default function About() {
                 {/* About Section */}
                 <p className="text-lg text-blue-400 mb-4">
                         My name is Barbara Montgomery, and I am the proud owner of 
-                        <strong> B’s Heavenly Sweets &amp; Eats</strong>! Baking and cooking have been a passion of 
+                        <strong> B&apos;s Heavenly Sweets &amp; Eats</strong>! Baking and cooking have been a passion of 
                         mine from an early age, inspired by watching and helping my grandma in the kitchen. 
                         I pursued my love for cooking by taking Home Economics from 7th to 12th grade to 
                         deepen my skills. My mother adored my baking, so while she cooked Sunday dinner, 
@@ -22,7 +22,7 @@ export default function About() {
                         these treats, and my business flourished beyond just strawberries!
                     </p>
                     <p className="text-lg text-blue-400 mb-4">
-                        Originally, my business was named <strong>B’s Heavenly Sweets</strong>. However, as 
+                        Originally, my business was named <strong>B&apos;s Heavenly Sweets</strong>. However, as 
                         demand for my cooking services grew, I expanded into savory dishes and added 
                         “Eats” to the name. Today, I offer not only delectable desserts for all occasions 
                         but also hearty, delicious meals for individuals and events. It brings me great joy 

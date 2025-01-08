3fc0c76da42663a53e0402570557218d28d2255b
zijunw04 <floatsinkfc@gmail.com>
2025-01-07 19:46:42 -0600

    demo

--- src/app/api/create-checkout-session/route.js
+++ src/app/api/create-checkout-session/route.js
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-});
 
 export async function POST(request) {
   try {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2023-10-16',
+    });
+    
     const { email, cartItems } = await request.json();
 
     const line_items = cartItems.map((item) => {
